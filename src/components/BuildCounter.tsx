9966e0ed4a5a7e0ee160ce202e2d63e156913d4c
billgobills <LagunitaIPA@gmail.com>
2023-12-07 13:58:47 -0500

    feat: BuildCounter

---
+++ src/components/BuildCounter.tsx
@@ -0,0 +1,8 @@
+const count = 0;
+const style = {
+  height: "30px",
+  width: "30px",
+};
+
+export const BuildCounter = () => <div style={style}>{count}</div>;
+BuildCounter.displayName = "BuildCounter";
